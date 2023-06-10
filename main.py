f8da806d410bd8bc23b60028e6f350e04cf13f83
Astijus <astijuskarvauskas@Astijuss-Air.broadband>
2023-06-10 13:56:00 +0100

    Base project with pygame setup

---
+++ main.py
@@ -0,0 +1,26 @@
+# Example file showing a circle moving on screen
+import pygame
+
+# pygame setup
+pygame.init()
+screen = pygame.display.set_mode((512, 512))
+clock = pygame.time.Clock()
+running = True
+dt = 0
+
+while running:
+    # poll for events
+    # pygame.QUIT event means the user clicked X to close your window
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    # flip() the display to put your work on screen
+    pygame.display.flip()
+
+    # limits FPS to 60
+    # dt is delta time in seconds since last frame, used for framerate-
+    # independent physics.
+    dt = clock.tick(60) / 1000
+
+pygame.quit()
