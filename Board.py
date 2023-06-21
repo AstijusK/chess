380498413e26baf55ad56a7ebcbb608aa9168c34
Astijus <astijuskarvauskas@Astijuss-Air.broadband>
2023-06-21 13:20:18 +0100

    Removed 'States' enum, replaced it with 'Type' enum in the 'Piece' class

--- Board.py
+++ Board.py
@@ -1,28 +1,16 @@
 from enum import Enum
+from Piece import Piece
 
 
 class Board:
-    class States(Enum):
-        EMPTY = 0
-        W_PAWN = 1
-        W_KNIGHT = 2
-        W_BISHOP = 3
-        W_ROOK = 4
-        W_KING = 5
-        W_QUEEN = 6
-        B_PAWN = 7
-        B_KNIGHT = 8
-        B_BISHOP = 9
-        B_ROOK = 10
-        B_KING = 11
-        B_QUEEN = 12
+    # In the future, make separate classes for each piece,
+    # and instantiate objects from those classes instead of general class.
 
-    def __init__(self):
-        self.squares = [[10, 8, 9, 11, 12, 9, 8, 10],
-                        [7, 7, 7, 7, 7, 7, 7, 7],
-                        [0, 0, 0, 0, 0, 0, 0, 0],
-                        [0, 0, 0, 0, 0, 0, 0, 0],
-                        [0, 0, 0, 0, 0, 0, 0, 0],
-                        [0, 0, 0, 0, 0, 0, 0, 0],
-                        [1, 1, 1, 1, 1, 1, 1, 1],
-                        [4, 2, 3, 6, 5, 3, 2, 4]]
+    squares = [[Piece(10), Piece(8), Piece(9), Piece(11), Piece(12), Piece(9), Piece(8), Piece(10)],
+               [Piece(7), Piece(7), Piece(7), Piece(7), Piece(7), Piece(7), Piece(7), Piece(7)],
+               [Piece(0), Piece(0), Piece(0), Piece(0), Piece(0), Piece(0), Piece(0), Piece(0)],
+               [Piece(0), Piece(0), Piece(0), Piece(0), Piece(0), Piece(0), Piece(0), Piece(0)],
+               [Piece(0), Piece(0), Piece(0), Piece(0), Piece(0), Piece(0), Piece(0), Piece(0)],
+               [Piece(0), Piece(0), Piece(0), Piece(0), Piece(0), Piece(0), Piece(0), Piece(0)],
+               [Piece(1), Piece(1), Piece(1), Piece(1), Piece(1), Piece(1), Piece(1), Piece(1)],
+               [Piece(4), Piece(2), Piece(3), Piece(6), Piece(5), Piece(3), Piece(2), Piece(4)]]
