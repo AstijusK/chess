8a0d22c298554f5298f0cda006dbad5c8cb41554
Astijus <astijuskarvauskas@Astijuss-Air.broadband>
2023-07-01 21:44:14 +0100

    fixed a bug where one click was being registered as multiple. also allowing for the deselection of pieces now.

--- main.py
+++ main.py
@@ -12,8 +12,10 @@ clock = pygame.time.Clock()
 running = True
 dt = 0
 
-status = 0
+# Keeps track of whether piece a piece has been clicked on and 'primed' for movement
+selected = 0
 piece_to_move = None
+counter = 0
 
 while running:
     # poll for events
@@ -22,18 +24,16 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-        if pygame.mouse.get_pressed()[0]:
-            if status == 0:
+
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            if selected == 0:
                 mouse_pos = pygame.mouse.get_pos()
                 square_x = math.trunc(mouse_pos[0] / 64)
                 square_y = math.trunc(mouse_pos[1] / 64)
 
                 if Board.squares[square_y][square_x].PieceType is not Piece.Type.EMPTY:
                     piece_to_move = Board.squares[square_y][square_x]
-                    status = 1
-                    print("Piece selected: ", piece_to_move.PieceType)
-                else:
-                    print("Can't select empty square")
+                    selected = 1
             else:
                 mouse_pos2 = pygame.mouse.get_pos()
                 square_x2 = math.trunc(mouse_pos2[0] / 64)
@@ -41,10 +41,8 @@ while running:
 
                 if Board.squares[square_y2][square_x2] is not piece_to_move:
                     Board.squares[square_y2][square_x2] = piece_to_move
-
                     Board.squares[square_y][square_x] = Piece(0)
-                    status = 0
-                    print("Placing selected piece")
+                selected = 0
 
     # flip() the display to put your work on screen
     pygame.display.flip()
@@ -54,7 +52,6 @@ while running:
     dark = (125, 135, 150)
     light = (232, 235, 239)
     colour = 1
-    selected = False
 
     # Sprite loading
     # BLACK PIECES
@@ -77,16 +74,15 @@ while running:
     for x in range(9):
         for y in range(9):
             if colour == 0:
-                pygame.draw.rect(screen, dark, (0 + (64 * x), 0 + (64 * y), 64, 64))
+                pygame.draw.rect(screen, dark, (64 * x, 64 * y, 64, 64))
                 colour = 1
             else:
-                pygame.draw.rect(screen, light, (0 + (64 * x), 0 + (64 * y), 64, 64))
+                pygame.draw.rect(screen, light, (64 * x, 64 * y, 64, 64))
                 colour = 0
 
     # Reading board states and drawing pieces
     for x in range(0, len(board.squares)):
         for y in range(0, len(board.squares)):
-
             match Board.squares[x][y].PieceType:
                 # White
                 case Piece.Type.W_PAWN:
@@ -117,18 +113,8 @@ while running:
                     screen.blit(b_queen, (0 + (64 * y), 0 + (64 * x)))
 
     # Handling piece movement
-        # Method 1: Click once on piece to move, click again on destination square
+        # Method 1: Click once on piece to move, click again on destination square [DONE]
         # Method 2: Mouse hold and drag a piece from starting square to destination square
-
-    # Steps
-        # Get coordinate position of mouse on-click
-        # Calculate starter square is being pressed from coordinates of mouse on-click
-        # Calculate destination square from coordinates of mouse on-click
-
-    # Don't worry about if move is legal yet, just get the positioning of pieces right.
-
-
-
 
 
 
