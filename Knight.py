77f59742bacc5a1e9d37463367667e859f03b7c4
Astijus <astijuskarvauskas@Astijuss-Air.broadband>
2023-07-02 14:26:12 +0100

    Filled in 'legal_moves' array for each piece type

--- Knight.py
+++ Knight.py
@@ -6,9 +6,12 @@ class Knight(Piece):
     def __init__(self, piece_type):
         super().__init__(piece_type)
         self.legal_moves = [
-            (),
-            (),
-            (),
-            ()
+            (1, 2),
+            (-1, 2),
+            (2, 1),
+            (-2, 1),
+            (-2, -1),
+            (2, -1),
+            (-1, -2),
+            (1, -2)
         ]
-
